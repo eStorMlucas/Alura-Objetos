320996c3f24b53450100777a83174451d0aaf220
eStorMLucas <lpCruz04@hotmail.com>
2023-05-16 16:57:23 -0300

    Fim 1° Aula

---
+++ alterar.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: 'Jorge',
+    profissão: 'Policial',
+}
+
+pessoa.telefone = '10 210101010'
+
+console.log(pessoa.telefone)
+
+pessoa.telefone = '20 320202020'
+
+console.log(pessoa)
--- objeto.js
+++ objeto.js
@@ -1,7 +1,17 @@
 const pessoa = {
     nome: 'Lucas',
     idade: 19,
-    cpf: 22231399984
+    cpf: '22231399984',
+    email: 'lucas@dominio.com'
 }
 
-console.log(pessoa.cpf)
+console.log
+    (` 
+    O nome do cliente é ${pessoa.nome}, sua idade é ${pessoa.idade}, o três primeiros digitos de seu cpf são ${pessoa.cpf.substring(0, 3)}
+    `)
+
+const props = ['nome', 'cpf', 'email', 'altura', 'idade']
+
+props.forEach(propriedade => {
+    console.log(`A propriedade ${propriedade} tem valor ${pessoa[propriedade]}`)
+});
